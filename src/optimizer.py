5ea3a5a10ee7f93f8552d6d166391bb9a05481be
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-07-06 12:27:10 -0300

    Add Adam optimizer

--- src/optimizer.py
+++ src/optimizer.py
@@ -13,7 +13,7 @@ class Optimizer(ABC):
         pass
 
     @abstractmethod
-    def apply(self, layer_number: int, learning_date: float, dw: np.ndarray) -> np.ndarray:
+    def apply(self, layer_number: int, learning_date: float, epoch_number: int, dw: np.ndarray) -> np.ndarray:
         """Calculates the delta-weights matrix to apply for a layer and returns it."""
         pass
 
@@ -27,28 +27,22 @@ class GradientDescentOptimizer(Optimizer):
     def parse(config=None):
         pass
 
-    def apply(self, layer_number: int, learning_date: float, dw: np.ndarray) -> np.ndarray:
+    def apply(self, layer_number: int, learning_date: float, epoch_number: int, dw: np.ndarray) -> np.ndarray:
         return learning_date * dw
 
 
 class MomentumOptimizer(Optimizer):
     """A simple optimizer that adds a percentage of the previous iteration's delta_w to make a momentum effect."""
 
-    def __init__(self, alpha) -> None:
+    def __init__(self, alpha: float=0.8) -> None:
         self.alpha = alpha
         if self.alpha <= 0 or self.alpha >= 1:
             print(f"⚠️⚠️⚠️ Warning: MomentumOptimizer received alpha outside of range (0, 1): {self.alpha}")
 
-    def parse(config: dict):
-        if 'alpha' not in config or config['alpha'] is None:
-            raise Exception('MomentumOptimizer requires an alpha parameter')
-        alpha = float(config['alpha'])
-        return MomentumOptimizer(alpha=alpha)
-
     def initialize(self, network: Network):
         self.previous_dw = [np.zeros_like(weights) for weights in network.layer_weights]
 
-    def apply(self, layer_number: int, learning_date: float, dw: np.ndarray) -> np.ndarray:
+    def apply(self, layer_number: int, learning_date: float, epoch_number: int, dw: np.ndarray) -> np.ndarray:
         dw_matrix =  learning_date * dw + self.alpha * self.previous_dw[layer_number]
         self.previous_dw[layer_number] = dw_matrix
         return dw_matrix
@@ -57,25 +51,47 @@ class MomentumOptimizer(Optimizer):
 class RMSPropOptimizer(Optimizer):
     """An optimizer based on root mean squares."""
 
-    def __init__(self, gamma, epsilon=1e-8) -> None:
-        self.epsilon = epsilon if epsilon is not None else 1e-8
+    def __init__(self, gamma: float=0.9, epsilon: float=1e-8) -> None:
+        self.epsilon = epsilon
         self.gamma = gamma
         if self.gamma <= 0 or self.gamma >= 1:
             print(f"⚠️⚠️⚠️ Warning: RMSPropOptimizer received alpha outside of range (0, 1): {self.gamma}")
         if self.epsilon <= 0 or self.epsilon >= 1:
             print(f"⚠️⚠️⚠️ Warning: RMSPropOptimizer received negative or large epsilon: {self.epsilon}")
 
-    def parse(config: dict):
-        if 'gamma' not in config or config['gamma'] is None:
-            raise Exception('RMSPropOptimizer requires an gamma parameter')
-        epsilon = float(config['epsilon']) if 'epsilon' in config else None
-        gamma = float(config['gamma'])
-        return RMSPropOptimizer(gamma=gamma, epsilon=epsilon)
-
     def initialize(self, network: Network):
         self.previous_s_matrix = [np.zeros_like(weights) for weights in network.layer_weights]
 
-    def apply(self, layer_number: int, learning_date: float, dw: np.ndarray) -> np.ndarray:
+    def apply(self, layer_number: int, learning_date: float, epoch_number: int, dw: np.ndarray) -> np.ndarray:
         s_matrix =  self.gamma * self.previous_s_matrix[layer_number] + (1 - self.gamma) * np.square(dw)
         self.previous_s_matrix[layer_number] = s_matrix
         return learning_date / np.sqrt(s_matrix + self.epsilon) * dw
+
+
+class AdamOptimizer(Optimizer):
+    """An optimizer that combines the concepts of RMSProp and Momentum."""
+
+    def __init__(self, beta1: float=0.9, beta2: float=0.999, epsilon: float=1e-8) -> None:
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        if self.beta1 < 0 or self.beta1 >= 1:
+            print(f"⚠️⚠️⚠️ Warning: AdamOptimizer received beta1 outside of range [0, 1): {self.beta1}")
+        if self.beta2 < 0 or self.beta2 >= 1:
+            print(f"⚠️⚠️⚠️ Warning: AdamOptimizer received beta2 outside of range [0, 1): {self.beta2}")
+        if self.epsilon <= 0 or self.epsilon >= 1:
+            print(f"⚠️⚠️⚠️ Warning: AdamOptimizer received negative or large epsilon: {self.epsilon}")
+
+    def initialize(self, network: Network):
+        self.m_per_layer = [np.zeros_like(weights) for weights in network.layer_weights]
+        self.v_per_layer = [np.zeros_like(weights) for weights in network.layer_weights]
+
+    def apply(self, layer_number: int, learning_date: float, epoch_number: int, dw: np.ndarray) -> np.ndarray:
+        self.m_per_layer[layer_number] = self.beta1 * self.m_per_layer[layer_number] + (1 - self.beta1) * dw
+        self.v_per_layer[layer_number] = self.beta2 * self.v_per_layer[layer_number] + (1 - self.beta2) * np.square(dw)
+
+        m_hat = self.m_per_layer[layer_number] / (1 - np.power(self.beta1, epoch_number))
+        v_hat = self.v_per_layer[layer_number] / (1 - np.power(self.beta2, epoch_number))
+
+        dw_matrix = learning_date * m_hat / (np.sqrt(v_hat) + self.epsilon)
+        return dw_matrix
--- src/trainer.py
+++ src/trainer.py
@@ -16,7 +16,7 @@ class NetworkTrainer:
 
         self.optimizer.initialize(network)
 
-    def evaluate_and_adjust(self, input: np.ndarray, expected_output: np.ndarray) -> list[np.ndarray]:
+    def evaluate_and_adjust(self, input: np.ndarray, expected_output: np.ndarray, epoch_number: int) -> list[np.ndarray]:
         if input.ndim != 1:
             raise ValueError("The input must have only 1 dimention")
         if len(input) != self.network.input_size:
@@ -48,7 +48,7 @@ class NetworkTrainer:
         # Calculate delta weights matrices
         dw_matrix_per_layer = [None] * self.network.layer_count
         for i in range(self.network.layer_count):
-            dw_matrix_per_layer[i] = columnarize(self.optimizer.apply(i, self.learning_rate, np.concatenate((np.ones(1), outputs_per_layer[i]))[:, None] * s_vector_per_layer[i]))
+            dw_matrix_per_layer[i] = columnarize(self.optimizer.apply(i, self.learning_rate, epoch_number, np.concatenate((np.ones(1), outputs_per_layer[i]))[:, None] * s_vector_per_layer[i]))
 
         return dw_matrix_per_layer
 
@@ -58,7 +58,7 @@ class NetworkTrainer:
             epoch += 1
             weights_adjustments = [np.zeros_like(w) for w in self.network.layer_weights]
             for i in range(len(dataset)):
-                add_lists(weights_adjustments, self.evaluate_and_adjust(dataset[i], expected_outputs[i]))
+                add_lists(weights_adjustments, self.evaluate_and_adjust(dataset[i], expected_outputs[i], epoch))
             self.network.adjust_weights(weights_adjustments)
 
             outputs = [self.network.evaluate(d) for d in dataset]
@@ -66,3 +66,26 @@ class NetworkTrainer:
             print(f"Error after epoch {epoch}: {err}")
             if err <= acceptable_error:
                 break
+
+    def train_analyze(self, dataset: list[np.ndarray], expected_outputs: list[np.ndarray], acceptable_error, max_epochs=None):
+        epoch = 0
+        error_history = []
+        error_history.append(self.error_function.error_for_dataset(np.array(expected_outputs), np.array([self.network.evaluate(d) for d in dataset])))
+        finish_reason = 'unspecified'
+        while (True):
+            epoch += 1
+            weights_adjustments = [np.zeros_like(w) for w in self.network.layer_weights]
+            for i in range(len(dataset)):
+                add_lists(weights_adjustments, self.evaluate_and_adjust(dataset[i], expected_outputs[i], epoch))
+            self.network.adjust_weights(weights_adjustments)
+
+            outputs = [self.network.evaluate(d) for d in dataset]
+            err = self.error_function.error_for_dataset(np.array(expected_outputs), np.array(outputs))
+            error_history.append(err)
+            if err <= acceptable_error:
+                finish_reason = 'Acceptable error reached'
+                break
+            if max_epochs is not None and epoch >= max_epochs:
+                finish_reason = 'Max epochs reached'
+                break
+        return epoch, finish_reason, error_history
